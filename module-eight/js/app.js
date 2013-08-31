4893cc2ff338626329d2a6138329819e057ff16e
unknown <mc.gevero@gmail.com>
2013-08-31 13:21:47 -0700

    added module-eight

---
+++ module-eight/js/app.js
@@ -0,0 +1,5 @@
+
+var apikey = "8xqmw9tqaxj2bzvgm7xvm7hb";
+var baseUrl = "http://api.rottentomatoes.com/api/public/v1.0";	
+var moviesSearchUrl = baseUrl + '/movies.json?apikey=' + apikey;
+var movies;
